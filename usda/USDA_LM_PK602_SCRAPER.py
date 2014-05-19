07a6ed5dbfbd117834d40bf38a041e2e81694119
Bill Cary <bill.cary@gmail.com>
2014-05-18 23:49:33 -0400

    Additional updates

--- usda/USDA_LM_PK602_SCRAPER.py
+++ usda/USDA_LM_PK602_SCRAPER.py
@@ -5,12 +5,20 @@ Created on Sun May 18 00:04:55 2014
 @author: billcary
 """
 
-from lxml import etree
+from lxml import etree, objectify
 
 file_name = './data/LM_PK602_sample_lmr_ws.xml'
-root = etree.parse(file_name)
 
-report_dates = root.list()[0].list()
+with open(file_name) as f:
+    xml = f.read()
+    
+root = objectify.fromstring(xml)
+
+print root.attrib
+
+for report_date in root.report.iterchildren():
+    print report_date.attrib['report_date']
+
 
 desired_aggregate_figures = ['Cutout and Primal Values', 'Current Volume']
 pork_cuts= ['Loin Cuts', 'Butt Cuts', 'Picnic Cuts', 'Ham Cuts', 'Belly Cuts', \
@@ -31,10 +39,17 @@ variety_cuts = []
 ai_cuts = []
 
 
-for date in report_dates:
-    for report in date:
-        values = report.list()
+for report_date in root.report.iterchildren():
+    date = report_date.attrib['report_date']
+    print date
+    for report in report_date.iterchildren():
+        print report.attrib['label']
         if report.attrib['label'] == 'Cutout and Primal Values':
-            primal_cutout.append(date, values.attrib('pork_carcass'), values.attrib('pork_loin'), \
-            values.attrib('pork_loin'), values.attrib('pork_picnic'), values.attrib('pork_rib'), \
-            values.attrib('pork_ham'), values.attrib('pork_belly'))
+            primal_cutout.append([date, report.record.attrib['pork_carcass'], report.record.attrib['pork_loin'], \
+            report.record.attrib['pork_butt'], report.record.attrib['pork_picnic'], report.record.attrib['pork_rib'], \
+            report.record.attrib['pork_ham'], report.record.attrib['pork_belly']])
+        elif report.attrib['label'] == 'Current Volume':
+            loads.append([date, report.record.attrib['temp_cuts_total_load'], \
+            report.record.attrib['temp_process_total_load']])
+        elif report.attrib['label'] in pork_cuts:
+            
